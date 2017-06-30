93a38211afa6aad4429499fc698973322c9e5e52
iPadisthin <albert@Alberts-MacBook-Pro.local>
2017-06-30 14:30:20 +1000

    how to

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
     Barba.Prefetch.init();
 
-    $('.explain-menu-toggle').click(function(){
-        if($(this).hasClass('open')){
+    $('.explain-menu-toggle').click(function () {
+        if ($(this).hasClass('open')) {
             $('.explain button, .explain-menu').removeClass('open');
             $('.overlay, .help').fadeOut();
         } else {
@@ -12,27 +12,28 @@ $(document).ready(function(){
             $('.explain-menu').addClass('open');
         }
     });
-    $('.explain-menu button').click(function(){
-        if(!$(this).hasClass('open')){
+    $('.explain-menu button').click(function () {
+        if (!$(this).hasClass('open')) {
             $(this).siblings().removeClass('open');
             $('.help').fadeOut();
             $(this).addClass('open');
             var target = $(this).attr('data-target');
-            $('.overlay, #'+target).fadeIn();
+            $('.overlay, #' + target).fadeIn();
         }
     });
-    $('.howto-menu-toggle').click(function(){
-        $('.explain button, .explain-menu').removeClass('open');
-        if($(this).hasClass('open')){ 
+    $('.howto-menu-toggle').click(function () {
+        if ($(this).hasClass('open')) {
+            $('.explain button, .explain-menu').removeClass('open');
             $('.overlay, .help').fadeOut();
         } else {
-            $('.overlay, .help').fadeOut();
+            $('.explain button, .explain-menu').removeClass('open');
+            $('.help').fadeOut();
             $(this).addClass('open');
             var target = $(this).attr('data-target');
-            $('.overlay, #'+target).fadeIn();
+            $('.overlay, #' + target).fadeIn();
         }
     });
-    $('.overlay').click(function(){
+    $('.overlay').click(function () {
         $('.overlay, .help').fadeOut();
         $('.explain button, .explain-menu').removeClass('open');
     });
