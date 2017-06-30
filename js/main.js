0815ccdd927e79da8019140d894aacbde9be4f01
iPadisthin <albert@Alberts-MacBook-Pro.local>
2017-06-30 14:08:05 +1000

    explain menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
     Barba.Prefetch.init();
-     
+
     $('.explain-menu-toggle').click(function(){
         if($(this).hasClass('open')){
             $('.explain button, .explain-menu').removeClass('open');
             $('.overlay, .help').fadeOut();
         } else {
-            $('.explain-menu-toggle, .explain-menu').addClass('open');
+            $('.explain button').removeClass('open');
+            $('.overlay, .help').fadeOut();
+            $(this).addClass('open');
+            $('.explain-menu').addClass('open');
         }
     });
     $('.explain-menu button').click(function(){
         if(!$(this).hasClass('open')){
             $(this).siblings().removeClass('open');
             $('.help').fadeOut();
+            $(this).addClass('open');
+            var target = $(this).attr('data-target');
+            $('.overlay, #'+target).fadeIn();
+        }
+    });
+    $('.howto-menu-toggle').click(function(){
+        $('.explain button, .explain-menu').removeClass('open');
+        if($(this).hasClass('open')){ 
+            $('.overlay, .help').fadeOut();
+        } else {
+            $('.overlay, .help').fadeOut();
             $(this).addClass('open');
             var target = $(this).attr('data-target');
             $('.overlay, #'+target).fadeIn();
